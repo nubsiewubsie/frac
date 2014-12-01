d5df89ac7d47d195a921e4213aa12703ce803a05
nubsiewubsie <soccer_932@rogers.com>
2014-12-01 11:16:39 -0500

    V06
    Menu made
    cleaned up parsing and reduce fcns

--- zobail frac/Zobail_frac_V01.cpp
+++ zobail frac/Zobail_frac_V01.cpp
@@ -33,40 +33,42 @@ void deleteFrac (frac *a, int deletePosition, int n){
 
 void reduce (frac *exp,int position){
 	int r = 0;
-	int numerator = exp[position].f1.n;
-	int denominator = exp[position].f1.d;
+	frac *c = exp;
+	int *p = &position;
+	int numerator = c[*p].f1.n;
+	int denominator = c[*p].f1.d;
 		//printf ("%i, %i",numerator,denominator);
 	 	do
 	    {
-	        r = exp[position].f1.n % exp[position].f1.d;
+	        r = c[*p].f1.n % c[*p].f1.d;
 	        if(r == 0)
 	            break;
-	        exp[position].f1.n = exp[position].f1.d;
-	        exp[position].f1.d = r;
+	        c[*p].f1.n = c[*p].f1.d;
+	        c[*p].f1.d = r;
 	    }
    		while(true);
    		
-   	exp[position].f1.n = numerator/exp[position].f1.d;
-   	exp[position].f1.d = denominator/exp[position].f1.d; 	
+   	c[*p].f1.n = numerator/c[*p].f1.d;
+   	c[*p].f1.d = denominator/c[*p].f1.d; 	
 	 
 	r = 0;   
-	numerator = exp[position].f2.n;
-	denominator = exp[position].f2.d;
+	numerator = c[*p].f2.n;
+	denominator = c[*p].f2.d;
 		//printf ("%i, %i",numerator,denominator);
 	 	do
 	    {
-	        r = exp[position].f2.n % exp[position].f2.d;
+	        r = c[*p].f2.n % c[*p].f2.d;
 	        if(r == 0)
 	            break;
-	        exp[position].f2.n = exp[position].f2.d;
-	        exp[position].f2.d = r;
+	        c[*p].f2.n = c[*p].f2.d;
+	        c[*p].f2.d = r;
 	    }
    		while(true); 
    		
-   	exp[position].f2.n = numerator/exp[position].f2.d;
-   	exp[position].f2.d = denominator/exp[position].f2.d; 
+   	c[*p].f2.n = numerator/c[*p].f2.d;
+   	c[*p].f2.d = denominator/c[*p].f2.d; 
 		
-   	printf ("\n%i/%i %i/%i\n",exp[position].f1.n,exp[position].f1.d,exp[position].f2.n,exp[position].f2.d);
+   	printf ("\n%i/%i %i/%i\n",c[*p].f1.n,c[*p].f1.d,c[*p].f2.n,c[*p].f2.d);
 }
 
 int rb (int min, int max)
@@ -81,9 +83,10 @@ int rb (int min, int max)
         if (s[i] == '(' && s[i+1] == '/') return 0;
         if (s[i] == '/' && s[i+1] == ')') return 0;
         if (s[i] == ')' && s[i+1] == '(') return 0;
-    }*/
+	}
+}*/
  
-int walkAdd (char s[], int*i , char end){ //broken
+int walkAdd (char s[], int*i , char end){
     int n=0;
     int neg = 0;
     (*i)++;
@@ -112,43 +115,91 @@ int parse (char s[],int position,frac *exp){
     int j = 0;
     int count = 0;
   	printf ("%s\n",s);
+  	frac *c = exp;
+  	int *p = &position;
   	
     for (i = 0; s[i]; i++){ 
 		
         if (s[i] == '(' && count == 0)
-        	{exp[position].f1.n = walkAdd (s, &i, '/');
+        	{c[*p].f1.n = walkAdd (s, &i, '/');
         	count++;}
         if (s[i] == '/' && count == 1)
-             {exp[position].f1.d = walkAdd (s, &i, ')');
+             {c[*p].f1.d = walkAdd (s, &i, ')');
              count++;}
         if (s[i] == ')' && s[i+2]=='(')  
-             {if (s[i] == '+') exp[position].op = ADD;
-             if (s[i] == '-') exp[position].op = SUB;
-             if (s[i] == '/') exp[position].op = DIV;
-             if (s[i] == '*') exp[position].op = MULT;
+             {if (s[i] == '+') c[*p].op = ADD;
+             if (s[i] == '-') c[*p].op = SUB;
+             if (s[i] == '/') c[*p].op = DIV;
+             if (s[i] == '*') c[*p].op = MULT;
 			 i+=2;}             
         if (s[i] == '(' && count == 2)
-             {exp[position].f2.n =  walkAdd (s, &i, '/');
+             {c[*p].f2.n =  walkAdd (s, &i, '/');
              count ++;}             
         if (s[i] == '/' && count == 3)
-             exp[position].f2.d = walkAdd (s, &i,')');
+             c[*p].f2.d = walkAdd (s, &i,')');
     }
-    printf ("\n%i/%i %i/%i\n",exp[position].f1.n,exp[position].f1.d,exp[position].f2.n,exp[position].f2.d);
+    printf ("\n%i/%i %i/%i\n",c[*p].f1.n,c[*p].f1.d,c[*p].f2.n,c[*p].f2.d);
 }
 
 void sortVal (int numExp, frac * exp){
 	int i;
 	for (i=numExp;i>numExp;i++)
 	{
-		reduce (exp,i);
+		//reduce (exp,i);
 	}
 }
 
 int main (){
 	frac exp [NUM_EXPRESSIONS];
 	int numExp =1;
+	int leave = 0;
+	char choice;
 	char string [80] = "(54/-54)+(88/-1)";
-	parse (string,1,exp);
-	reduce (exp,1);
-	system ("PAUSE");   
+	//parse (string,1,exp);
+	//reduce (exp,1);
+	printf ("Fraction program\nChoose an operation\na - exit\t\tb - sort by value\nc - sort by operator\td - sort by answer\n");
+	printf ("e - enter expression\tf - get random expression\ng - view expressions\th - view answers\ni - delete expression\n");
+	do
+	{
+		fflush (stdin);
+		scanf ("%c",&choice);
+		switch (choice)
+		{
+		case 'a' :
+			printf ("Good bye\n");
+			leave = 1;
+			break;
+		case 'b' :
+			printf ("Sorting by Value\n");
+			break;
+		case 'c' :
+			printf ("Sorting by operator\n");
+			break;
+		case 'd' :
+			printf ("Getting random expression\n");
+			break;
+		case 'e' :
+			printf ("Printing expressions\n");
+			break;
+		case 'f' :
+			printf ("Get random expression\n");
+			break;
+		case 'g' :
+			printf ("View expression\n");
+			break ;
+		case 'h' :
+			printf ("Displaying expressions");
+			break;
+		default :
+			printf ("Incorrect input\n");
+			printf ("Fraction program\nChoose an operation\na - exit\t\tb - sort by value\nc - sort by operator\td - sort by answer\n");
+			printf ("e - enter expression\tf - get random expression\ng - view expressions\th - reduce fractions\ni - delete expression\n");
+			break;
+		}
+	}while (leave==0);
+	//system ("PAUSE");   
 }
+
+
+
+
